73ca5eadd0933a0f2bbae45fd0a7a2a99d965e48
Ben Shaver <benpshaver@gmail.com>
2023-04-24 19:11:35 -0400

    fix: fixed bug where env::current_dir would interpret name of the current directory as a file name in PathBuf.file_name() and therefore couldn't find files in the cwd

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
+use log::trace;
 use std::env;
 use std::error::Error;
 use std::fmt;
 use std::path::PathBuf;
-
-mod doctest;
 
 #[derive(Debug)]
 pub enum FileLookupError {
@@ -23,32 +22,49 @@ impl fmt::Display for FileLookupError {
 }
 
 pub fn find_file(file: &str, under: &PathBuf) -> Result<Option<PathBuf>, FileLookupError> {
+    let file: PathBuf = file.into();
+    // Check if the file is in the cwd
+    if file.exists() {
+        trace!("Found {:?} in cwd", &file);
+        return Ok(Some(file));
+    };
     // Find the current working directory
     let cwd = match env::current_dir() {
         Ok(path) => path,
         Err(_) => return Err(FileLookupError::CwdNotFound),
     };
-    // Iterate up through parent directories
+    // If you tried to check for the file in the cwd now, you'd have some trouble
+    // In my test, env::current_dir() interprets the name of immedaite dir you're
+    // in as a file, not a directory. But since we first checked in the cwd,
+    // we can start going up the tree immediately:
     for dir in cwd.ancestors() {
+        trace!("Looking in {:?}", dir);
         // If we're above the `under` directory (e.g. $HOME),
         // stop looking
         if !dir.starts_with(under) {
+            trace!("File {:?} not found under {:?}", &file, dir);
             return Ok(None);
         }
         // Check for file
-        let file_path = dir.with_file_name(file);
+        // TODO: Could possibly use set_file_name here to avoid extra allocation
+        let file_path = dir.with_file_name(&file);
+        trace!("Checking for existence of path {:?}", file_path);
         if file_path.exists() {
             return Ok(Some(file_path));
         }
     }
     // Search terminates
+    trace!("File {:?} not found anywhere", file);
     Ok(None)
 }
 
 pub fn home_find_file(file: &str) -> Result<Option<PathBuf>, FileLookupError> {
     // Find the home directory
     let home: PathBuf = match dirs::home_dir() {
-        Some(path) => path,
+        Some(path) => {
+            trace! {"Found home directory {:?}", &path};
+            path
+        }
         None => return Err(FileLookupError::HomeDirNotFound),
     };
 
@@ -58,8 +74,9 @@ pub fn home_find_file(file: &str) -> Result<Option<PathBuf>, FileLookupError> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
-    fn home_find_file_basic() {
+    fn home_find_file_cargo_toml() {
         home_find_file("Cargo.toml").unwrap();
     }
 }
